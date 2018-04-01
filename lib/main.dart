66acc51fc6d67258dfb4b0194b3dcac72ff8992a
nasum <tomato.wonder.life@gmail.com>
2018-04-01 19:07:50 +0900

    add firebase-auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,27 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() => runApp(new MyApp());
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+void main() => runApp(new MyHomePage());
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  FirebaseUser user;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -33,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
             appBar: new AppBar(
               title: new Text('Flutter firebase playground'),
             ),
+            drawer: _createDrawer(user),
             body: new ListView(
                 shrinkWrap: true,
                 padding: const EdgeInsets.all(20.0),
@@ -72,5 +69,58 @@ class _MyHomePageState extends State<MyHomePage> {
                                 )
                               ])))
                 ])));
+  }
+
+  void _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser siginUser = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    setState(() {
+      user = siginUser;
+    });
+  }
+
+  void _handleSignOut() {
+    _auth.signOut();
+    setState(() {
+      user = null;
+    });
+  }
+
+  Widget _createDrawer(FirebaseUser user) {
+    Text displayName = new Text('Guest');
+    Text email = new Text('Guest');
+    CircleAvatar avator = new CircleAvatar(
+      backgroundColor: Colors.blue,
+      child: new Text('G'),
+    );
+
+    if (user != null) {
+      displayName = new Text(user.displayName);
+      email = new Text(user.email);
+      avator =
+          new CircleAvatar(backgroundImage: new NetworkImage(user.photoUrl));
+    }
+    return new Drawer(
+        child: new ListView(
+      children: <Widget>[
+        new UserAccountsDrawerHeader(
+            accountName: displayName,
+            accountEmail: email,
+            currentAccountPicture: avator),
+        new ListTile(
+          title: new Text('Sigin In Google'),
+          leading: new Icon(FontAwesomeIcons.google),
+          onTap: _handleSignIn,
+        ),
+        new ListTile(
+          title: new Text('Sigin Out Google'),
+          onTap: _handleSignOut,
+        )
+      ],
+    ));
   }
 }
